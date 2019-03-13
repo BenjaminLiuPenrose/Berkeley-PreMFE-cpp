a8226aba0fdc9d50b2a240a184ffae7ce1a5c98f
BenjaminLiu <imbenliu@gmail.com>
2019-03-13 10:46:21 +0800

    add hw2-4

---
+++ Assignment5+/agents.h
@@ -0,0 +1,8 @@
+//
+// Created by Benjamin on 3/12/2019.
+//
+
+#ifndef ASSIGNMENT5_AGENTS_H
+#define ASSIGNMENT5_AGENTS_H
+
+#endif //ASSIGNMENT5_AGENTS_H
---
+++ Assignment5/utils.h
@@ -0,0 +1,8 @@
+//
+// Created by Benjamin on 3/4/2019.
+//
+
+#ifndef ASSIGNMENT5_UTILS_H
+#define ASSIGNMENT5_UTILS_H
+
+#endif //ASSIGNMENT5_UTILS_H
